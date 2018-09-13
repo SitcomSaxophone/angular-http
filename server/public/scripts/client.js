c6988208ed05d9ed364012bc55c90e6f8dff5c0a
Samuel Solberg <pauliesays65@gmail.com>
2018-09-13 11:24:21 -0500

    server GET shaking hands with client GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,16 @@
 const myApp = angular.module('MyApp', []);
 
-myApp.controller('HttpController', [function () {
+myApp.controller('HttpController', ['$http',function ($http) {
     let self = this;
 
     self.message = 'Hello';
+
+    $http({
+        method: 'GET',
+        url: '/shells'
+    }).then(function(response) {
+        console.log(response);
+    }).catch( function() {
+        console.log(error);
+    })
 }])
--- server/server.js
+++ server/server.js
@@ -4,9 +4,14 @@ const app = express();
 
 const port = 5000;
 
+let shells = [{type: 'Conch', color: 'pink'}, {type: 'Blue', color: 'blue'}];
 
 app.use(express.static('server/public'));
 app.use(bodyParser.json());
+
+app.get('/shells', (req, res) => {
+    res.sendStatus(200);
+});
 
 app.listen(port, () => {
     console.log('listening on port: ', port);
